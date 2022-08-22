dff78be9066a38ce338b5d31afc5dc374a8c459b
ExplainIT <explaint@gmail.com>
2022-08-22 13:17:24 +0200

    Added gitignore file, added calculate fucntion

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def calculate(a, b):
+    c = a + b
+    return c
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+    calculate()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
